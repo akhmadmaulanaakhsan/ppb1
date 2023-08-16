5851d388c2b6648134c22a042d53ae67faeb0428
akhmadmaulanaakhsan <maulanaakhsan10@gmail.com>
2023-08-16 10:16:07 +0700

    add new branch

--- app/src/main/java/com/example/ppb1/MainActivity.kt
+++ app/src/main/java/com/example/ppb1/MainActivity.kt
@@ -7,8 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-add comment
         //comment
         //comment2
+
+
+        //new develop branch
     }
 }
